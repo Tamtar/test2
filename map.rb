2dac1a47894b2705f6ab6c6416e912c8d10b6952
Tamtar <tamtarthegreat@gmail.com>
2013-01-21 18:11:45 -0700

    changed Map class

--- map.rb
+++ map.rb
@@ -1,25 +1,25 @@
 class Map
 	@x
 	@y
-	@map_dimensions
 	@grid
 	def initialize
 		@x = 20
 		@y = 20
-		@map_dimensions = [@x, @y]
+		@grid = Array.new[@x, @y]
 	end
 	def dimensions
-		return @map_dimensions 
+		return [@x, @y] 
 	end
 	def set_rewards
-		@grid[5, 7] = true
+		@grid[5, 7] = "test"
 	end
 	def reward?(x, y)
-		if @grid[x, y] == true
-			return true
-		else
-			return false
-		end
+		puts @grid[5, 7]
+		# if @grid[x, y] == true
+		# 	return true
+		# else
+		# 	return false
+		# end
 	end
 
 end

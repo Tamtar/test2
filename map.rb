371825b8eb65de5932cf137a3ec93e1c41558e5c
Joel Dehlin <joel.dehlin@gmail.com>
2013-01-22 02:40:45 +0100

    turned array to hash

--- map.rb
+++ map.rb
@@ -5,23 +5,21 @@ class Map
 	def initialize
 		@x = 20
 		@y = 20
-    @grid = Array.new(20) {Array.new(20) {false}}
-		#@grid = Array.new[@x, @y]
+    @grid = Hash.new(false)
 	end
 	def dimensions
 		return [@x, @y]
 	end
 	def set_rewards
-		@grid[5, 7] = true
+		@grid[[5, 7]] = true
+		return true
 	end
 	def reward?(x, y)
-		puts "@x: #{@x}"
-		#puts @grid[5, 7]
-		# if @grid[x, y] == true
-		# 	return true
-		# else
-		# 	return false
-		# end
+		if @grid[[x, y]] == true
+			return "true!"
+		else
+			return "false!"
+		end
 	end
 
 end
--- test.rb
+++ test.rb
@@ -2,5 +2,5 @@ load 'map.rb'
 
 map1 = Map.new
 puts map1.dimensions
-puts map1.set_rewards
-puts map1.reward?(4,5)
+map1.set_rewards
+puts map1.reward?(5,8)

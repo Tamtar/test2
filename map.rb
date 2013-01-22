28a328a989d24622ec086a4642dcbbbd17434b5c
Joel Dehlin <joel.dehlin@gmail.com>
2013-01-22 02:30:09 +0100

    2-dimensional ray working

--- map.rb
+++ map.rb
@@ -5,16 +5,18 @@ class Map
 	def initialize
 		@x = 20
 		@y = 20
-		@grid = Array.new[@x, @y]
+    @grid = Array.new(20) {Array.new(20) {false}}
+		#@grid = Array.new[@x, @y]
 	end
 	def dimensions
-		return [@x, @y] 
+		return [@x, @y]
 	end
 	def set_rewards
-		@grid[5, 7] = "test"
+		@grid[5, 7] = true
 	end
 	def reward?(x, y)
-		puts @grid[5, 7]
+		puts "@x: #{@x}"
+		#puts @grid[5, 7]
 		# if @grid[x, y] == true
 		# 	return true
 		# else

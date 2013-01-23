0a1b384b5d487c12716078a4c74b71237467da44
Tamtar <tamtarthegreat@gmail.com>
2013-01-22 23:45:27 -0700

    added move options to the player class

--- player.rb
+++ player.rb
@@ -2,10 +2,37 @@ class Player
 	@name
 	@x_location
 	@y_location
+	@direction
+
+	def initialize
+		@x_location = 10
+		@y_location = 10
+	end
 
 	def name?
 		puts "what is your name?"
 		@name = gets
 		puts "Hello " + @name
+	end
+
+	def direction?
+		puts "what direction would you like to go?"
+		puts "possible choices are up, down, left, or right. Type done to end."
+		@direction = gets.chop
+		if @direction == "up"
+			@x_location = @x_location+1
+		elsif @direction == "down"
+			@x_location = @x_location-1
+		elsif @direction == "right"
+			@y_location = @y_location+1			
+		elsif @direction == "left"
+			@y_location = @y_location-1
+		elsif @direction == "done"
+			puts "you have opted to be done"
+			return
+		else
+			"you can't do that"
+		end
+		puts "you are now at location (#{@x_location}, #{@y_location})"
 	end
 end
--- test.rb
+++ test.rb
@@ -8,8 +8,9 @@ player = Player.new
 
 # puts map1.dimensions
 # puts map1.set_rewards
-puts map1.reward?(4,5)
+# puts map1.reward?(4,5)
 player.name?
+player.direction?
 
 # puts "map dimensions are #{map1.dimensions}"
 # map1.set_rewards
